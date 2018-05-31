05659e6ac24f672187f7c4780622cb457fbdc186
André Agostinho <andre.f.agostinho@tecnico.ulisboa.pt>
2018-05-31 15:47:32 +0100

    Did the paste and wait apps. Updated the makefile accordingly.
    Paste and wait apps take a region from where to paste and a size to
    paste.

---
+++ apps/paste.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <time.h>
+
+#define mperror(err) do { char __buf__[1024]; sprintf(__buf__, "%s:%d %s(), errno = %d", __FILE__, __LINE__-1, __func__, err); perror(__buf__); } while(0);
+#define emperror(err) do { mperror(err); exit(err); } while(0);
+
+#include "libclipboard.h"
+
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int r = 0;
+    long int region;
+    long int size;
+
+
+	if (argc < 2 || argc > 4) return EXIT_FAILURE;
+
+    region = strtol(argv[2], NULL, 0);
+    size = strtol(argv[3], NULL, 0);
+
+    char* buf = malloc(size);
+	int cb = clipboard_connect(argv[1]);
+	if (cb == -1) emperror(errno);
+
+    r = clipboard_paste(cb, region, buf, (int)size);
+    if (r == 0) { mperror(errno)}
+    else {
+        printf("Pasted from clipboard region %ld: \n\"%s\"\n", region, buf);
+    }
+
+	clipboard_close(cb);
+
+	return EXIT_SUCCESS;
+}
